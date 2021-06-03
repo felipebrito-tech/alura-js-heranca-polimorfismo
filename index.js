e12fbc65b7d645d0affdc1d891ccf6bfb5bcb967
felipebrito-tech <felipe.brito87@gmail.com>
2021-06-03 00:57:29 -0300

    Implementação da Herança.

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import {Conta} from "./Conta.js"
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new Conta(0, 1001, cliente1);
+const contaCorrenteRicardo = new ContaCorrente(0, 1001, cliente1);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
