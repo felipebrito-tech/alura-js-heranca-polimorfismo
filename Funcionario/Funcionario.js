37f70b7f8fb4cc62d899c5615659895f5076e047
felipebrito-tech <felipe.brito87@gmail.com>
2021-06-08 19:35:09 -0300

    Autenticação do Cliente via interface e polimorfismo.

--- Funcionario/Funcionario.js
+++ Funcionario/Funcionario.js
@@ -7,8 +7,8 @@ export class Funcionario{
         this._bonificacao = 1;
     }
 
-    get senha(){
-        return this._senha;
+    autenticar(senha){
+        return this._senha == senha;
     }
 
     cadastrarSenha(senha){
--- index.js
+++ index.js
@@ -8,8 +8,11 @@ diretor.cadastrarSenha("123456789");
 const gerente = new Gerente("Ricardo", 5000, 12345678901);
 gerente.cadastrarSenha("1234");
 
+const cliente = new Cliente("Laís", 98765432100, "12345");
 const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456789");
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "1234");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "12345");
 
 console.log(diretorEstaLogado);
 console.log(gerenteEstaLogado);
+console.log(clienteEstaLogado);
